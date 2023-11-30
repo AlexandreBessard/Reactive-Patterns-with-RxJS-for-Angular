de328926fea95662aaca3d56e60001cdf66d3ab1
Alexandre Bessard <alexandre.bessard@gmail.com>
2023-11-30 16:51:01 -0500

    combined 2 streams

--- Chapter06/recipes-book-front/src/app/core/services/recipes.service.ts
+++ Chapter06/recipes-book-front/src/app/core/services/recipes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject } from 'rxjs';
+import {BehaviorSubject, tap} from 'rxjs';
 import { Recipe } from '../model/recipe.model';
 import { environment } from 'src/environments/environment';
 const BASE_PATH = environment.basePath
@@ -11,8 +11,20 @@ const BASE_PATH = environment.basePath
 
 export class RecipesService {
 
-  recipes$ = this.http.get<Recipe[]>(`${BASE_PATH}/recipes`);
-  private filterRecipeSubject = new BehaviorSubject<Recipe>({title: '' });
+  recipes$ = this.http.get<Recipe[]>(`${BASE_PATH}/recipes`).pipe(
+      tap(() => console.log('GET request executed for recipes'))
+  );
+
+  // Read-only stream
+  private filterRecipeSubject = new BehaviorSubject<Recipe>(
+      // initial value is emitted to any subscribers when they subscribe to the observable
+      // hold a current value
+      // empty object
+      {title: '' });
+/*
+This is a common pattern in RxJS when you want to expose an observable externally but restrict
+certain operations that are available on the original subject, like modifying its value.
+ */
   filterRecipesAction$ = this.filterRecipeSubject.asObservable();
 
   constructor(private http: HttpClient) { }
--- Chapter06/recipes-book-front/src/app/recipes-list/recipes-list.component.ts
+++ Chapter06/recipes-book-front/src/app/recipes-list/recipes-list.component.ts
@@ -12,12 +12,25 @@ import { combineLatest } from 'rxjs';
 })
 export class RecipesListComponent implements OnInit {
 
+  // all recipes, stream data, emit new value
   recipes$ = this.service.recipes$;
   /* The readonly stream */
+  // when filter emits new value, the recipes list should be updated accordingly
   filterRecipesAction$ = this.service.filterRecipesAction$;
-  filtredRecipes$ = combineLatest([this.recipes$, this.filterRecipesAction$]).pipe(
+
+  filtredRecipes$ =
+      // combine the latest values from two observables, recipes$ and filterRecipesAction$
+      // This means that whenever either this.recipes$ or
+      // this.filterRecipesAction$ emits a new value, the combineLatest will emit an array
+      // containing the latest values from both observables.
+      combineLatest([this.recipes$, this.filterRecipesAction$])
+    .pipe(
+        // array contains 2 elements coming from recipes$ and filterRecipesAction$
     map(([recipes, filter]: [Recipe[], Recipe]) => {
+      // filtering recipes array based on whether their titles contain the text specified in the 'filter' object
+      // The filter() method creates a shallow copy  of a portion of a given array,
       return recipes.filter(recipe => recipe.title?.toLowerCase()
+          // return index of the first occurrence
       .indexOf(filter?.title?.toLowerCase() ?? '') != -1)
     })
   );
